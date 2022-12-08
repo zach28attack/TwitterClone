d0df57c695b7133a003de1a433782e974a85fef6
Zachary Casares <rrumble250@gmail.com>
2022-12-08 15:57:32 -0500

    route to index after successful tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,7 +25,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
+        format.html { redirect_to tweets_path, notice: "Tweet was successfully created." }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :index, status: :unprocessable_entity }
