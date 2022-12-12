ade4bc55735cf348863fe1d9e0c5aeb1bfbb1c79
Zachary Casares <rrumble250@gmail.com>
2022-12-12 18:07:34 -0500

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tweets, except: [:edit,:update]
   resources :users
+  get 'home', to: "home#home"
   root "tweets#index"
   #root "home#home"
 end
