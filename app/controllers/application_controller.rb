e924c0f4fb8ce84f41721d5c4e419c22d4fd4c1d
Zachary Casares <rrumble250@gmail.com>
2022-12-27 13:48:55 -0500

    permit username in registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # this code is the 'easy way' to whitelist parameters when using devise 
+  # here -->
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+  # <--to here
 end
