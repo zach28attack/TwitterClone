29ef3e381629f884316f9f0cb138d6848d8bd913
Zachary Casares <rrumble250@gmail.com>
2022-12-27 16:37:25 -0500

    add username length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets 
-  
+  validates :username, uniqueness: {case_sensitive: false}
+  validates_length_of :username, in: 3..15
 end
