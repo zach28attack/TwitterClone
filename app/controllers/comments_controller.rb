3ac40761f610d75f9fa09ddf9cd2f32668e98dd9
Zachary Casares <rrumble250@gmail.com>
2023-01-02 15:32:40 -0500

    create comment action in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,24 @@
-class CommentsController < ApllicationController
-  def index
-  end
+class CommentsController < ApplicationController
   def create
-    
+    @comment = current_user.comments.new(comment_params)
+    @comment.save
+    redirect_to tweet_path(id: params[:tweet_id])
   end
+
   def edit
     
   end
+
   def update
     
   end
 
   def destroy
     
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body).merge(tweet_id: params[:tweet_id])
   end
 end
