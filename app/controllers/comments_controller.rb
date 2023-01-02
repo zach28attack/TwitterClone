5fef5ce9f8fa20e338b6ea55ded8b1bb0778f1b3
Zachary Casares <rrumble250@gmail.com>
2023-01-02 14:57:41 -0500

    reate comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApllicationController
+  def index
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+end
