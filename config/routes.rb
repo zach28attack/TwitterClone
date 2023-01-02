623f38d6dbd7877f9766647094e41b98d91afae8
Zachary Casares <rrumble250@gmail.com>
2023-01-02 15:33:59 -0500

    add post comment routes inside of tweets resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweets, except: [:edit,:update] do
+    resources :comments, only: %i{create}
     member do
       post :retweet
     end
