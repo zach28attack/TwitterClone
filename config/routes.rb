056caa37889e3bcba5a97a43ffd49bbc9a490268
Zachary Casares <rrumble250@gmail.com>
2023-01-03 18:07:00 -0500

    add like: create and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tweets, except: [:edit,:update] do
+  resources :tweets, except: %i[edit update] do
     resources :comments, only: %i{create}
     member do
       post :retweet
     end
   end
-
+  resources :likes, only: %i[create destroy]
+  
   get 'home', to: "home#home"
   root "tweets#index"
-  #root "home#home"
-end
+ end
