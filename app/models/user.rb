60fd47c7e1fa25da7c6e67bdf0f208330bccbb08
Zachary Casares <rrumble250@gmail.com>
2023-01-06 12:36:19 -0500

    add dependants on deletion of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets 
-  has_many :comments
-  has_many :likes
+  has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :username, uniqueness: {case_sensitive: false}
   validates_length_of :username, in: 3..15
