d41523db8bbd51a1e922be3d02c20757e82e8dc1
Zachary Casares <rrumble250@gmail.com>
2023-01-06 14:44:59 -0500

    add delete comment dependent when tweet is deleted

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
   belongs_to :tweet, optional: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, as: :likeable
 
   validates :body, length: { maximum: 240 }, allow_blank: false, unless: :tweet_id
