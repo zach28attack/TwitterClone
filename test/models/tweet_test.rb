219a4b21ca04c48bffcaa62ad99bcefde6e12186
Zachary Casares <rrumble250@gmail.com>
2023-01-07 18:19:14 -0500

    add body validation test

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -3,7 +3,13 @@ require "test_helper"
 class TweetTest < ActiveSupport::TestCase
   test "should not create tweet without user" do
     tweet = Tweet.new
-    tweet.user_id = 1
     assert_not tweet.save, "Tweet is able to save without user association"
+  end
+
+  test "should not save if body is less than 240 characters" do
+    tweet = Tweet.new
+    tweet.user_id = 1
+    tweet.body = ""
+    assert_not tweet.save, ":body length validation not being enforced"
   end
 end
