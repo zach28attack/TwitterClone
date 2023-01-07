ba18e897fd6187030dcaf216cf2716a99ada7844
Zachary Casares <rrumble250@gmail.com>
2023-01-07 11:29:15 -0500

    add log output

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module TwitterClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.logger = Logger.new(STDOUT)
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
