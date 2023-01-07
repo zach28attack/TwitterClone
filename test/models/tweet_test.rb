7ea121877b2c941ef11ab8ec396998e2614b16ac
Zachary Casares <rrumble250@gmail.com>
2023-01-07 11:29:45 -0500

    add simple test

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create tweet without user" do
+    tweet = Tweet.new
+    tweet.user_id = 1
+    assert_not tweet.save, "Tweet is able to save without user association"
+  end
 end
