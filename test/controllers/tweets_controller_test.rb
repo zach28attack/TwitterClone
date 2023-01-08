864c6419fedb8bdf5405cbbdca2796441956ebff
Zachary Casares <rrumble250@gmail.com>
2023-01-08 12:07:47 -0500

    modified generated tweet controller tests

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -1,48 +1,52 @@
 require "test_helper"
 
 class TweetsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     @tweet = tweets(:one)
-#   end
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @tweet = tweets(:tweet)
+    @user = User.create(username: "gordo", email: "test@email.com", password: "password")
+    
+  end
 
-#   test "should get index" do
-#     get tweets_url
-#     assert_response :success
-#   end
+  test "should get index" do
+    get tweets_url
+    assert_response :success
+  end
 
-#   test "should get new" do
-#     get new_tweet_url
-#     assert_response :success
-#   end
+  test "should get new" do
+    get new_tweet_url
+    assert_response :success
+  end
 
-#   test "should create tweet" do
-#     assert_difference("Tweet.count") do
-#       post tweets_url, params: { tweet: {  } }
-#     end
+  test "should create tweet" do
+    assert_difference("Tweet.count") do  
+      sign_in @user  
+      post tweets_url, params: { tweet: { user_id: 1, body: "test body"} }
+    end
 
-#     assert_redirected_to tweet_url(Tweet.last)
-#   end
+    assert_redirected_to tweets_url
+  end
 
-#   test "should show tweet" do
-#     get tweet_url(@tweet)
-#     assert_response :success
-#   end
+  test "should show tweet" do
+    get tweet_url(@tweet)
+    assert_response :success
+  end
 
-#   test "should get edit" do
-#     get edit_tweet_url(@tweet)
-#     assert_response :success
-#   end
+  # test "should get edit" do
+  #   get edit_tweet_url(@tweet)
+  #   assert_response :success
+  # end
 
-#   test "should update tweet" do
-#     patch tweet_url(@tweet), params: { tweet: {  } }
-#     assert_redirected_to tweet_url(@tweet)
-#   end
+  # test "should update tweet" do
+  #   patch tweet_url(@tweet), params: { tweet: { body: "new body of test tweet" } }
+  #   assert_redirected_to tweet_url(@tweet)
+  # end
 
-#   test "should destroy tweet" do
-#     assert_difference("Tweet.count", -1) do
-#       delete tweet_url(@tweet)
-#     end
+  test "should destroy tweet" do
+    assert_difference("Tweet.count", -1) do
+      delete tweet_url(@tweet)
+    end
 
-#     assert_redirected_to tweets_url
-#   end
+    assert_redirected_to tweets_url
+  end
 end
