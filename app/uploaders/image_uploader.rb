fd6c62d883f38d71904ad5d6e6c2b88a268d50ee
Zachary Casares <rrumble250@gmail.com>
2023-01-11 16:26:06 -0500

    add shrine derivation code

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,13 @@
 class ImageUploader < Shrine
-
+  require "image_processing/mini_magick"
+    Attacher.derivatives do |original|
+      magick = ImageProcessing::MiniMagick.source(original)
+   
+      { 
+        large:  magick.resize_to_limit!(800, 800),
+        medium: magick.resize_to_limit!(500, 500),
+        small:  magick.resize_to_limit!(300, 300),
+      }
+    end
+ 
 end
