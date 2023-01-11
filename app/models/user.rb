474da34977e44f3aa930dbd9b90dce4008feb350
Zachary Casares <rrumble250@gmail.com>
2023-01-11 16:19:00 -0500

    code cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :username, uniqueness: {case_sensitive: false}
+  validates_length_of :username, in: 3..15
 
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  validates :username, uniqueness: {case_sensitive: false}
-  validates_length_of :username, in: 3..15
-
-  validates :name, presence: true
-
-  has_many :photos, dependent: :destroy
+  has_many :photos, class_name: "Photo", dependent: :destroy
 end
