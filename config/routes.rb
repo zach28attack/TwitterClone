e33b2093311ebdece855a5cb78c809041caeedb4
Zachary Casares <rrumble250@gmail.com>
2023-02-03 16:06:50 -0500

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :photos, only: %i[create show new destroy]
   devise_for :users
-  delete "users/:id", to: "users#destroy"
+  resources :photos, only: %i[create show new destroy]
   resources :users, only: %i[show]
   resources :tweets, except: %i[edit update] do
-    resources :comments, only: %i{create}
+    resources :comments, only: %i{create destroy}
     member do
       post :retweet
     end
   end
   resources :likes, only: %i[create destroy]
   
+  delete "users/:id", to: "users#destroy"
   get 'home', to: "home#home"
   root "tweets#index"
  end
