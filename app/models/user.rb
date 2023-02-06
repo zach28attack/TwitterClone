98d2926da328d9a965b0a905fd90e2113b4360d2
Zachary Casares <rrumble250@gmail.com>
2023-02-06 15:40:20 -0500

    associate user model with follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :photos, class_name: "Photo", dependent: :destroy
+  
+  has_many :follows
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
 end
