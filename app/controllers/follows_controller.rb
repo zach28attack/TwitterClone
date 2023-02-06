29b44ccaaabc879ee697f02987774a4dae4479fa
Zachary Casares <rrumble250@gmail.com>
2023-02-06 17:27:40 -0500

    add create action

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,7 +1,10 @@
 class FollowsController < ApplicationController
 
   def create
-   
+    @follow = Follow.new(follow_params)
+    if @follow.save
+      redirect_to user_path(User.find(params[:id]))
+    end
   end
 
   def destroy
