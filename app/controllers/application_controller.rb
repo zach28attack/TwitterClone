5f9cae0b1fc56329167c4ff5bea6b4e48786b144
Zachary Casares <rrumble250@gmail.com>
2023-02-10 18:33:44 -0500

    define @q var. for entire app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   # this code is the 'easy way' to whitelist parameters when using devise 
-  # here -->
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_q
+
+  def set_q
+    @q = User.ransack(params[:id])
+  end
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
   end
