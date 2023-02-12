1c603d8917719bd59a7ba82b40d9ed1a4ac9b96d
Zachary Casares <rrumble250@gmail.com>
2023-02-12 13:25:13 -0500

    clean up code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,26 +2,18 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[create destroy]
 
-  # GET /photos/1 or /photos/1.json
-  def show
-  end
-
-  # GET /photos/new
   def new
     @photo = Photo.new
-    
   end
 
-
-  # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)
-    @photo = Photo.new(image: @photo.image)
     @photo.image_derivatives!
     @photo.user_id = current_user.id
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to user_path(current_user), notice: "Photo was successfully created." }
+        current_user.photos.first.destroy if current_user.photos.count >= 2 #delete previously uploaded image upon new photo upload
+        format.html { redirect_to user_path(current_user), notice: "Profile picture successfully changed." }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -30,23 +22,19 @@ class PhotosController < ApplicationController
     end
   end
 
-  # DELETE /photos/1 or /photos/1.json
   def destroy
     @photo.destroy
-
     respond_to do |format|
-      format.html { redirect_to photos_url, notice: "Photo was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to photos_url, notice: "Photo was successfully deleted." }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_photo
       @photo = Photo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def photo_params
       params.require(:photo).permit(:title, :image, :user_id)
     end
