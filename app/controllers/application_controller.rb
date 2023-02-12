d497a0fb009a718a241a908e972d0f3043c3a9c8
Zachary Casares <rrumble250@gmail.com>
2023-02-12 12:37:05 -0500

    code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # this code is the 'easy way' to whitelist parameters when using devise 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_q
-
+  
   def set_q
     @q = User.ransack(params[:id])
   end
@@ -11,5 +11,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
   end
-  # <--to here
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,28 +7,15 @@ class CommentsController < ApplicationController
     redirect_to tweet_path(id: params[:tweet_id])
   end
 
-  def edit
-    #Remove these empty actions?
-  end
-
-  def update
-    
-  end
-
   def destroy
     @comment = Comment.find(params[:tweet_id])
     @comment.destroy
     respond_to do |format|
       format.html { redirect_to tweet_url, notice: "Comment was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:body).merge(tweet_id: params[:tweet_id])
