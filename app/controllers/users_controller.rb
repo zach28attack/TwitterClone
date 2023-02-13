470e0246bd7190c0842d15c22e372e3d58ce4902
Zachary Casares <rrumble250@gmail.com>
2023-02-13 15:14:15 -0500

    removed useless controller code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,39 +3,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new(user_params)
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "Welcome to the TwitterClone #{@user.username}, you have successfully signed up!"
-        redirect_to tweets_path
-    else
-        render 'new', status: :unprocessable_entity
-    end
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_path, notice: 'Account was deleted successfully.'
-  end
-
-  
 end
