5c16c27306d823e40589814298e16f46dda2ef1d
Zachary Casares <rrumble250@gmail.com>
2023-02-13 17:23:58 -0500

    clean up code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,13 +4,11 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
     resource_updated = update_resource(resource, account_update_params)
     yield resource if block_given?
     if resource_updated
       flash[:notice] = "Profile updated" # my contribution
       bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
-
       respond_with resource, location: after_update_path_for(resource)
     else
       clean_up_passwords resource
