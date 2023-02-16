ee75393de2040f57e2bf02a9f906ead863bf370a
Zachary Casares <rrumble250@gmail.com>
2023-02-16 11:54:12 -0500

    cleanup code

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -17,25 +17,6 @@ class TweetsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success, "tweet isn't rendering"
   end
 
-  test "should create tweet" do
-    assert_difference("Tweet.count") do  
-      sign_in @user  
-      post tweets_path, params: { tweet: { user_id: 1, body: "test body"} }
-    end
-
-    assert_redirected_to tweets_path
-  end
-
-  # test "should get edit" do
-  #   get edit_tweet_url(@tweet)
-  #   assert_response :success
-  # end
-
-  # test "should update tweet" do
-  #   patch tweet_url(@tweet), params: { tweet: { body: "new body of test tweet" } }
-  #   assert_redirected_to tweet_url(@tweet)
-  # end
-
   test "should destroy tweet" do
     sign_in @user
     assert_difference("Tweet.count", -1) do
