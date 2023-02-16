0e6cc9fa750e959439931c800fa98c59b8f1e6e3
Zachary Casares <rrumble250@gmail.com>
2023-02-16 15:20:05 -0500

    route to tweet index after signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
   end
+  def after_sign_out_path_for scope
+    tweets_path
+  end
 end
